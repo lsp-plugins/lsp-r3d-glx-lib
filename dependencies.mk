ba5778078f8f1c22fa08722f86c15ac66dd32b3d
sadko4u <sadko4u@gmail.com>
2020-05-04 00:48:18 +0300

    Updated dependencies

--- dependencies.mk
+++ dependencies.mk
@@ -2,7 +2,7 @@
 LSP_COMMON_LIB_VERSION     := 1.0.3
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_URL         := https://github.com/sadko4u/$(LSP_COMMON_LIB_NAME).git
-LSP_COMMON_LIB_TYPE        := hdr
+LSP_COMMON_LIB_TYPE        := src
 
 LSP_TEST_FW_VERSION        := 1.0.3
 LSP_TEST_FW_NAME           := lsp-test-fw
