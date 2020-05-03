2a262e3572dd9c33473932bfe5c5294eb699cc62
sadko4u <sadko4u@gmail.com>
2020-05-04 01:06:44 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -3,7 +3,7 @@ ARTIFACT_NAME               = lsp-r3d-glx-lib
 ARTIFACT_VARS               = LSP_R3D_GLX_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 0
-VERSION                     = 0.5.0-devel
+VERSION                     = 0.5.0
 
 # List of dependencies
 TEST_DEPENDENCIES = \
