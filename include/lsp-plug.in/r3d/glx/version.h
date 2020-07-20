a7f44c99b79d740089034aea7e84fccb017fb5ac
sadko4u <sadko4u@gmail.com>
2020-07-20 15:13:24 +0300

    Version up

--- include/lsp-plug.in/r3d/glx/version.h
+++ include/lsp-plug.in/r3d/glx/version.h
@@ -10,7 +10,7 @@
 
 #define LSP_R3D_GLX_LIB_MAJOR           0
 #define LSP_R3D_GLX_LIB_MINOR           5
-#define LSP_R3D_GLX_LIB_MICRO           1
+#define LSP_R3D_GLX_LIB_MICRO           2
 
 #ifdef LSP_R3D_GLX_LIB_BUILTIN
     #define LSP_R3D_GLX_LIB_EXPORT
