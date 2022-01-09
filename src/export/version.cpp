53a0a3b728ec1e309a8a0531d3ebd3caeb1a8a9a
sadko4u <sadko4u@gmail.com>
2022-01-09 22:07:29 +0300

    Updated project tree

--- src/export/version.cpp
+++ src/export/version.cpp
@@ -29,10 +29,9 @@ namespace lsp
     {
         namespace glx
         {
-            factory_t   factory;
-
-            // Builtin factory binding
-            LSP_R3D_BUILTIN_FACTORY(builtin_glx_factory, &lsp::r3d::glx::factory);
+            // Version function
+            LSP_R3D_GLX_LIB_CEXPORT
+            LSP_DEFINE_VERSION_FUNC(LSP_R3D_GLX_LIB)
         }
     }
 }
